5b759fa95abefa5d050020dba163de2a8fd8b17e
sntaranga <67263438+sntaranga@users.noreply.github.com>
2026-03-23 22:55:32 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,4 @@ provider "iosxe" {
   devices  = var.routers
 }
 
-resource "iosxe_interface_loopback" "management_loopback" {
-  for_each = { for router in var.routers : roiuter.name => router }
-  description                = "Management Loopback" 
-  ipv4_address               = "1.1.1.1" 
-  ipv4_address_mask          = "255.255.255.255" 
-  name                       = 1
-}
+
