4474dfcd7c154a1b854cf644f5c2ee532fabbbbc
sntaranga <67263438+sntaranga@users.noreply.github.com>
2026-03-23 22:33:45 +0100

    Add management loopback interface resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,10 @@ provider "iosxe" {
   password = "admin"
   url  = "https://10.1.1.73" 
 }
+
+resource "iosxe_interface_loopback" "management_loopback" {
+  description                = "Management Loopback" 
+  ipv4_address               = "1.1.1.1" 
+  ipv4_address_mask          = "255.255.255.255" 
+  name                       = 1
+}
